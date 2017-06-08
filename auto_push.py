00de18b901c52ba17845ba87bdaa0927336bc552
conrad <conrad.heidebrecht@gmail.com>
2017-06-08 10:37:18 -0400

    test

--- auto_push.py
+++ auto_push.py
@@ -6,9 +6,9 @@ projects = list(filter(None, os.popen('cd .. && ls').read().strip(' ').split('\n
 for p in projects:
     if p == 'Src' or p == 'Examples':
         projects.remove(p)
-
+print(projects)
 for p in projects:
-    os.system('cp -r $HOME/AndroidStudioProjects/%s/app/src %s/' % (p, p))
+    os.system('cp -r $HOME/AndroidStudioProjects/%s/app/src $HOME/AndroidStudioProjects/Src/%s/' % (p, p))
 
 os.system('git add -f .')
 commit_msg = input('Enter commit message: ')
