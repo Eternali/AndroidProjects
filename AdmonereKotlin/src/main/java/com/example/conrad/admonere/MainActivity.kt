4984a63d092674aa517b127f22fa429dce31bf11
conrad <conrad.heidebrecht@gmail.com>
2017-06-13 12:45:36 -0400

    test

--- AdmonereKotlin/src/main/java/com/example/conrad/admonere/MainActivity.kt
+++ AdmonereKotlin/src/main/java/com/example/conrad/admonere/MainActivity.kt
@@ -34,8 +34,173 @@ import java.util.Calendar
 internal var reminders : ArrayList<Reminder>? = null
 internal var filename : String = "reminders.xml"
 
-internal fun getReminders (ctx : Context, fname : String) : ArrayList<Reminder> {
 
+// gets the data stored in fname and returns it in a arraylist<Reminder>
+internal fun getReminders (ctx : Context, fname : String) : ArrayList<Reminder> {
+    var reminds : ArrayList<Reminder> = ArrayList()
+    val data: String
+    // get the data from the XML file and read it into a string
+    try {
+        val fis = ctx.openFileInput(fname)
+        val isr = InputStreamReader(fis)
+        val inBuff = CharArray(fis.available())
+        isr.read(inBuff)
+        data = String(inBuff)
+        Log.i("Read data", data)
+        isr.close()
+        fis.close()
+    } catch (e: FileNotFoundException) {
+        e.printStackTrace()
+        Log.i("Read Error", "Unable to find file")
+        return reminds
+    } catch (e: IOException) {
+        e.printStackTrace()
+        Log.i("Read Error", "IO Exception")
+        return reminds
+    }
+
+    // start parsing the XML data from the string (data)
+    val factory: XmlPullParserFactory
+    val xpp: XmlPullParser
+    var eventType: Int
+    try {
+        factory = XmlPullParserFactory.newInstance()
+        factory.isNamespaceAware = true
+        xpp = factory.newPullParser()
+        xpp.setInput(StringReader(data))
+        // used to determine location in XML file
+        eventType = xpp.eventType
+    } catch (e: XmlPullParserException) {
+        e.printStackTrace()
+        Log.i("XML Read Error", "Xml pull parser exception raised")
+        return reminds
+    }
+
+    // initialize all information variables
+    var date = ""
+    var time = ""
+    var contact = ""
+    var number = ""
+    var message = ""
+    var text = ""
+    // look through the XML file until the end is reached
+    while (eventType != XmlPullParser.END_DOCUMENT) {
+        try {
+            val name = xpp.name
+            when (eventType) {
+                XmlPullParser.START_TAG -> {
+                }
+                XmlPullParser.TEXT -> text = xpp.text
+                XmlPullParser.END_TAG ->
+                    // get information from the tag
+                    when (name) {
+                        "date" -> date = text
+                        "time" -> time = text
+                        "name" -> contact = text
+                        "number" -> number = text
+                        "message" -> message = text
+                        "reminder" -> reminds.add(Reminder(date.split("/").toTypedArray(),
+                                time.split(":").toTypedArray(), contact, number, message))
+                    }
+            }
+            eventType = xpp.next()
+        } catch (e: XmlPullParserException) {
+            e.printStackTrace()
+            Log.i("XML Read Error", "Xml pull parser exception raised")
+            return reminds
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.i("XML Read Error", "IO exception raised")
+            return reminds
+        }
+    }
+    return reminds
+}
+
+// takes the data stored in reminds and saves it to an xml file fname
+internal fun saveReminders (ctx : Context, fname : String, reminds : ArrayList<Reminder>) = try {
+    // create file if doesn't exist
+    // (FileOutputStream should create it automatically but just to be safe)
+    val filepath = ctx.filesDir.path + "/" + fname
+    val saveFile = File(filepath)
+    saveFile.createNewFile()
+    //            FileOutputStream fos = new FileOutputStream(saveFile);
+    val fos = ctx.openFileOutput(fname, Context.MODE_PRIVATE)
+    // create the serializer and writer objects
+    val xmlSerial = Xml.newSerializer()
+    val writer = StringWriter()
+    xmlSerial.setOutput(writer)
+    xmlSerial.startDocument("UTF-8", null)
+    xmlSerial.startTag(null, "reminders")
+    // for each reminder write its data into a XML structure
+    for ((date, time, name, number, message) in reminds) {
+        xmlSerial.startTag(null, "reminder")
+        xmlSerial.startTag(null, "date")
+        xmlSerial.text(TextUtils.join("/", date))
+        xmlSerial.endTag(null, "date")
+        xmlSerial.startTag(null, "time")
+        xmlSerial.text(TextUtils.join(":", time))
+        xmlSerial.endTag(null, "time")
+        xmlSerial.startTag(null, "name")
+        xmlSerial.text(name)
+        xmlSerial.endTag(null, "name")
+        xmlSerial.startTag(null, "number")
+        xmlSerial.text(number)
+        xmlSerial.endTag(null, "number")
+        xmlSerial.startTag(null, "message")
+        xmlSerial.text(message)
+        xmlSerial.endTag(null, "message")
+        xmlSerial.endTag(null, "reminder")
+    }
+    // end and write to document and close streams
+    xmlSerial.endTag(null, "reminders")
+    xmlSerial.endDocument()
+    xmlSerial.flush()
+    val dWrite = writer.toString()
+    fos.write(dWrite.toByteArray())
+    fos.close()
+} catch (e: IllegalArgumentException) {
+    Log.i("XML Write Error", "Illegal argument raised")
+    e.printStackTrace()
+} catch (e: IllegalStateException) {
+    Log.i("XML Write Error", "Illegal state of the document/streams")
+    e.printStackTrace()
+} catch (e: IOException) {
+    Log.i("XML Write Error", "IO exception raised")
+    e.printStackTrace()
+}
+
+internal fun orderReminders (reminds : ArrayList<Reminder>, order : Boolean) {
+    if (order) {
+        for (i in 0..reminds.size-1) {
+            var cal1 = Calendar.getInstance()
+            var tmp1 = reminds.get(i)
+            cal1.set(Calendar.YEAR, tmp1.date[2].toInt())
+            cal1.set(Calendar.MONTH, tmp1.date[1].toInt())
+            cal1.set(Calendar.DAY_OF_MONTH, tmp1.date[0].toInt())
+            cal1.set(Calendar.HOUR_OF_DAY, tmp1.time[0].toInt())
+            cal1.set(Calendar.MINUTE, tmp1.time[1].toInt())
+            cal1.set(Calendar.SECOND, 0)
+            for (j in i + 1..reminds.size - 1) {
+                val cal2 = Calendar.getInstance()
+                val tmp2 = reminds[j]
+                cal2.set(Calendar.YEAR, tmp2.date[2].toInt())
+                cal2.set(Calendar.MONTH, tmp2.date[1].toInt())
+                cal2.set(Calendar.DAY_OF_MONTH, tmp2.date[0].toInt())
+                cal2.set(Calendar.HOUR_OF_DAY, tmp2.time[0].toInt())
+                cal2.set(Calendar.MINUTE, tmp2.time[1].toInt())
+                cal2.set(Calendar.SECOND, 0)
+                if (order && cal1 > cal2) {
+                    reminds[i] = tmp2
+                    reminds[j] = tmp1
+                }
+                if (!order && cal1 < cal2) {
+                    reminds[i] = tmp2
+                    reminds[j] = tmp1
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -56,7 +221,7 @@ class MainActivity : AppCompatActivity () {
         context = applicationContext
         reminders = getReminders(context!!, filename)
         // create adapter that presents users with the reminders in a listview
-        adapter = RemindersArrayAdapter(this, reminders)
+        adapter = RemindersArrayAdapter(this, 0, reminders as ArrayList<Reminder>)
 
     }
 }
--- AdmonereKotlin/src/main/java/com/example/conrad/admonere/RemindersArrayAdapter.kt
+++ AdmonereKotlin/src/main/java/com/example/conrad/admonere/RemindersArrayAdapter.kt
@@ -17,16 +17,41 @@ import java.util.ArrayList
  * An array adapter of type Reminder and takes an xml file and fits the contents of each object
  * in the arraylist to the view
  */
-class RemindersArrayAdapter (var ctx : Context, var reminds : ArrayList<Reminder>)
-        : ArrayAdapter<Reminder> () {
+class RemindersArrayAdapter (var ctx : Context, var resource : Int, var reminds : ArrayList<Reminder>)
+        : ArrayAdapter<Reminder> (ctx, resource, reminds) {
 
     override fun notifyDataSetChanged() {
         super.notifyDataSetChanged()
-
+        orderReminders(reminds, true)
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        return super.getView(position, convertView, parent)
+        // inflate the remindView
+        var inflater : LayoutInflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var remindView : View? = if (convertView != null) inflater.inflate(R.layout.reminder_layout, parent, false)
+                                else null
+        // set components of the layout to values from the reminds array
+        val dateTime = remindView!!.findViewById(R.id.dateTime) as TextView
+        val contactName = remindView.findViewById(R.id.contactName) as TextView
+        val message = remindView.findViewById(R.id.message) as TextView
+        dateTime.text = "${reminds[position].time.joinToString(":")} ${reminds[position].date.joinToString("/")}"
+        contactName.text = reminds[position].name
+        message.text = reminds[position].message
+
+        // when a element is tapped, start an intent to EditActivity and put its data into it.
+        remindView!!.setOnClickListener({
+            val r = reminds[position]
+            val editIntent = Intent(context, EditActivity::class.java)
+            editIntent.putExtra("date", r.date.joinToString("/"))
+            editIntent.putExtra("time", r.time.joinToString(":"))
+            editIntent.putExtra("name", r.name)
+            editIntent.putExtra("number", r.number)
+            editIntent.putExtra("message", r.message)
+            editIntent.putExtra("index", position)
+            context.startActivity(editIntent)
+        })
+
+        return remindView
     }
 
 }
