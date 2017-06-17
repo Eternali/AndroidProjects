c5407158ba500af0a7ac980abb1f04f8e8f831f5
conrad <conrad.heidebrecht@gmail.com>
2017-06-17 16:49:17 -0400

    test

--- AdmonereKotlin/src/main/java/com/example/conrad/admonere/SettingsActivity.kt
+++ AdmonereKotlin/src/main/java/com/example/conrad/admonere/SettingsActivity.kt
@@ -3,13 +3,23 @@ package com.example.conrad.admonere
 // import required libraries
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Switch
 
 class SettingsActivity : Activity () {
 
+    // checked state of theme
+    var isDark : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_layout)
+
+        val darkSwitch : Switch = findViewById(R.id.darkSwitch) as Switch
+        darkSwitch.setOnCheckedChangeListener { compoundButton, b -> {
+            if (b) {
+                isDark = !isDark
+            }
+        } }
 
     }
 
