08965bb5e9eddf1da82f9975ce972a71abbbd4fe
conrad <conrad.heidebrecht@gmail.com>
2017-06-27 01:43:45 -0400

    test

--- AdmonereKotlin/src/main/java/com/example/conrad/admonere/SettingsActivity.kt
+++ AdmonereKotlin/src/main/java/com/example/conrad/admonere/SettingsActivity.kt
@@ -12,18 +12,18 @@ class SettingsActivity : Activity () {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // set the theme
-//        val isDark : Boolean = setTheme(this)
+        val isDark : Boolean = setTheme(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_layout)
 
         val darkSwitch : Switch = findViewById(R.id.darkSwitch) as Switch
-//        darkSwitch.isChecked = isDark
+        darkSwitch.isChecked = isDark
         darkSwitch.setOnCheckedChangeListener { compoundButton, b -> run {
             val sharePref : SharedPreferences = getPreferences(Context.MODE_PRIVATE)
             val spEditor : SharedPreferences.Editor = sharePref.edit()
             spEditor.putBoolean(getString(R.string.isdark), b)
             spEditor.commit()
-            recreate()
+//            recreate()
         } }
 
     }
