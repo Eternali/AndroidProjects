33632fdc0c0b66618f4df4fbe9afe44c8ef4f1b1
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-10-31 14:06:48 -0400

    working on syde161project

--- syde161proto01/src/main/java/com/example/fa11en/syde161proto01/SettingsActivity.kt
+++ syde161proto01/src/main/java/com/example/fa11en/syde161proto01/SettingsActivity.kt
@@ -10,7 +10,7 @@ class SettingsActivity : Activity () {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
     }
 
 }
