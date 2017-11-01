049856e62a6c3e6f6983f3dcc03979636cde102e
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2017-11-01 17:32:08 -0400

    working on syde161

--- syde161proto01/src/main/java/com/example/fa11en/syde161proto01/SettingsActivity.kt
+++ syde161proto01/src/main/java/com/example/fa11en/syde161proto01/SettingsActivity.kt
@@ -5,12 +5,41 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.RadioGroup
+import android.widget.ToggleButton
 
 class SettingsActivity : Activity () {
+
+    val displayToggleListener: RadioGroup.OnCheckedChangeListener = RadioGroup.OnCheckedChangeListener { group, checkedId ->
+        group.clearCheck()
+        for (r in 0..group.childCount) {
+            val view: ToggleButton = group.getChildAt(r) as ToggleButton
+            if (view.id == checkedId) group.check(checkedId)
+        }
+        Log.i("Toggle clicked", Integer.toString(checkedId))
+    }
+
+    lateinit var listToggle: ToggleButton
+    lateinit var calToggle: ToggleButton
+
+    fun toggleDisplay(view: View) {
+        val group = view.parent as RadioGroup
+        group.clearCheck()
+        group.check()
+        Log.i("Toggle clicked", Integer.toString())
+//        (view.parent as RadioGroup).check(view.id)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        listToggle = findViewById(R.id.listToggle)
+        calToggle = findViewById(R.id.calendarToggle)
+
+//        findViewById<RadioGroup>(R.id.displayToggleGroup).setOnCheckedChangeListener(displayToggleListener)
     }
 
 }
